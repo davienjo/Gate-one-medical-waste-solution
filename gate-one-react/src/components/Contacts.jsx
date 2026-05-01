36900de4dd74b32d350561d30b075997962be0be
David Thairu <thaividz2@gmail.com>
2026-05-01 07:45:09 +0300

    modified the contacts section and added ScrollToTop

--- gate-one-react/src/components/Contacts.jsx
+++ gate-one-react/src/components/Contacts.jsx
@@ -1,77 +1,105 @@
+
 import "./Contacts.css";
+import { useState } from "react";
 
 function Contacts(){
-  return(
-  <section className="contact-section">
-        <h2>Contact Us</h2>
-        <p>
-          Have questions or need assistance? Reach out to our team using the
-          information below.
-        </p>
+  
+  const[name, setName] = useState("");
+  const[email, setEmail] = useState("");
+  const[message, setMessage]= useState("");
 
-        <form
-          className="contact-form"
-          id="contactForm"
-          action="https://formsubmit.co/90c790c2a0f1aec9265573e028e181dd"
-          method="POST"
-        >
-          <div className="form-group">
-            <label for="name">Name</label>
-            <input
-              type="text"
-              placeholder="Your Name"
-              id="name"
-              name="name"
-              required
-            />
-          </div>
+const[isSubmitted, setIsSubmitted]= useState(false)
 
-          <div className="form-group">
-            <label for="email">Email</label>
-            <input
-              type="email"
-              placeholder="example@mail.com"
-              id="email"
-              name="email"
-              required
-            />
-          </div>
+  function handleSubmit(e) {
+    e.preventDefault()
+  
+    setIsSubmitted(true)
 
-          <div className="form-group">
-            <label for="message">Message</label>
-            <textarea
-              id="message"
-              name="message"
-              cols="20"
-              rows="5"
-              placeholder="Write Your Message"
-              required
-            ></textarea>
-          </div>
 
-          <button type="submit" className="submit-btn">Send Message</button>
+    setTimeout(() => {
+      setIsSubmitted(false)
+    }, 3000)
+  
+    alert("Message sent successfully");
 
-          <p id="successMsg">Message sent successfully!</p>
+setName("");
+setEmail("");
+setMessage("");
+  } 
+  return (
+    <section className="contact-section">
+      <h2>Contact Us</h2>
+      <p>
+        Have questions or need assistance? Reach out to our team using the
+        information below.
+      </p>
 
-          
-          <input type="hidden" name="_captcha" value="false" />
-        </form>
-
-        <div className="contact-info">
-          <div className="contact-item">
-            <p>Phone: <a href="tel:+254725818260">0725-818260</a></p>
-          </div>
-
-          <div className="contact-item">
-            <p>Email:pgngugi@gmail.com</p>
-          </div>
-
-          <div className="contact-item">
-            <p>123 Kabete, Nairobi, Kenya</p>
-          </div>
+      <form onSubmit={handleSubmit} className="contact-form" id="contactForm">
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            placeholder="Your Name"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            name="name"
+            required
+          />
         </div>
-      </section>
 
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            placeholder="example@mail.com"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            cols="20"
+            rows="5"
+            placeholder="Write Your Message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            required
+          ></textarea>
+        </div>
+
+        <button type="submit" className="submit-btn">
+          Send Message
+        </button>
+
+        {isSubmitted && <p id="successMsg">Message sent successfully!</p>}
+
+        <input type="hidden" name="_captcha" value="false" />
+      </form>
+
+      <div className="contact-info">
+        <div className="contact-item">
+          <p>
+            Phone: <a href="tel:+254725818260">0725-818260</a>
+          </p>
+        </div>
+
+        <div className="contact-item">
+          <p>Email:pgngugi@gmail.com</p>
+        </div>
+
+        <div className="contact-item">
+          <p>123 Kabete, Nairobi, Kenya</p>
+        </div>
+      </div>
+    </section>
   );
 }
 
